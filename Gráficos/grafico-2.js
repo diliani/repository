68399b5cf936fe00f047d36d167b95f3b8711e39
diliani <132509989+diliani@users.noreply.github.com>
2024-10-10 15:09:56 -0300

    ds

--- Gráficos/grafico-2.js
+++ Gráficos/grafico-2.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Municípios com maior número de comunidades quilombolas no Paraná',
+            text: 'Top 10 Carros Mais Baratos do Brasil',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
